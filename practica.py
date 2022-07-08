14e3c6576868798cbeffa76faa0f072a64ea8af8
lrxa <lixato2019@yandex.ru>
2022-07-08 21:21:48 +0300

    1

--- practica.py
+++ practica.py
@@ -1,105 +1,63 @@
+import speech_recognition
 import os
-import time
-import speech_recognition as sr
-from fuzzywuzzy import fuzz
 import webbrowser
 
-opts = {
-    "alias": ('практика'),
-    "cmds": {
-        "dispetcher": ('открой диспетчер задач', 'включи диспетчер задач', 'запусти диспетчер задач'),
-        "panel": ('открой панель управления', 'включи панель управления', 'запусти панель управления'),
-        "poisk": ('поиск в интернете', 'найди в интернете', 'ищи в интернете', 'выполни поиск в интернете', 'осуществи поиск в интрнете', 'сделай поиск в интернете'),
-        "vikl": ('выключись', 'прекрати работу', 'выключайся', 'завершай работу', 'заверши работу'),
-        "klav": ('открой клавиатуру', 'открой электронную клавиатуру', 'включи клавиатуру', 'включи электронную клавиатуру', 'запусти клавиатуру', 'запусти электронную клавиатуру'),
-        "sch": ('считай', 'считать'),
-        "comp": ('выключи компьютер', 'выключай компьютер')
-    }
-}
+recognition = speech_recognition.Recognizer()
+mic = speech_recognition.Microphone()
 
+def get_audio():
+    recognition = speech_recognition.Recognizer()
+    mic = speech_recognition.Microphone()
+    with mic as audio_file:
+        audio = recognition.listen(audio_file)
+        text = recognition.recognize_google(audio, language='ru-RU')
+        return text.lower()
 
-def callback(recognizer, audio):
-    try:
-        voice = recognizer.recognize_google(audio, language="ru-RU").lower()
-        print("Распознано: " + voice)
-
-        if voice.startswith(opts["alias"]):
-            cmd = voice
-
-            for x in opts['alias']:
-                cmd = cmd.replace(x, "").strip()
-
-            cmd = recognize_cmd(cmd)
-            execute_cmd(cmd['cmd'])
-
-    except sr.UnknownValueError:
-        print("Команда не распознана!")
-
-
-def recognize_cmd(cmd):
-    RC = {'cmd': '', 'percent': 0}
-    for c, v in opts['cmds'].items():
-
-        for x in v:
-            vrt = fuzz.ratio(cmd, x)
-            if vrt > RC['percent']:
-                RC['cmd'] = c
-                RC['percent'] = vrt
-
-    return RC
-
-
-def execute_cmd(cmd):
-    if cmd == 'dispetcher':
+while True:
+    print("Здравствуйте, повелитель")
+    print("Что желаете сделать?")
+    text = get_audio()
+    print("Вы сказали: " + text)
+    if (text.count("открой диспетчер задач") > 0) or (text.count("включи диспетчер задач") > 0) or (text.count("запусти диспетчер задач") > 0) or (text.count("открыть диспетчер задач") > 0) or (text.count("включить диспетчер задач") > 0) or (text.count("запустить диспетчер задач") > 0):
+        print("Открываю диспетчер задач")
         os.system("taskmgr")
-
-    if cmd == 'panel':
+        break
+    elif (text.count("открой панель управления") > 0) or (text.count("включи панель управления") > 0) or (text.count("запусти панель управления") > 0) or (text.count("открыть панель управления") > 0) or (text.count("включить панель управления") > 0) or (text.count("запустить панель управления") > 0):
+        print("Открываю панель управления")
         os.system("control")
-
-    if cmd == 'poisk':
-        with sr.Microphone() as source:
-            print("Что искать?")
-            audio = r.listen(source)
-            try:
-                voice = r.recognize_google(audio, language="ru-RU").lower()
-                print("Ищу в интернете: " + voice)
-                url = 'https://www.google.co.in/search?q='
-                search_url = url + voice
-                webbrowser.open(search_url)
-            except:
-                print("Не могу найти")
-
-    if cmd == 'klav':
+        break
+    elif (text.count("открой клавиатуру") > 0) or (text.count("открыть клавиатуру") > 0) or (text.count("открой экранную клавиатуру") > 0) or (text.count("открыть экранную клавиатуру") > 0) or (text.count("включи клавиатуру") > 0) or (text.count("включить клавиатуру") > 0) or (text.count("включи экранную клавиатуру") > 0) or (text.count("включить экранную клавиатуру") > 0) or (text.count("запусти экранную клавиатуру") > 0) or (text.count("запустить экранную клавиатуру") > 0) or (text.count("запусти клавиатуру") > 0) or (text.count("запустить клавиатуру") > 0):
+        print("Открываю экранную клавиатуру")
         os.system("osk")
-
-    if cmd == 'sch':
-        with sr.Microphone() as source:
-            print("Что считать?")
-            audio = r.listen(source)
-            try:
-                voice = r.recognize_google(audio, language="ru-RU").lower()
-                print("Считаю: " + voice)
-                s = voice.split()
-                if s[0].isdigit() == True:
-                    if s[1] == '-':
-                        if s[2].isdigit() == True:
-                            print(int(s[0]) - int(s[2]))
-                        else: print('второе число не распознано, не удалось посчитать')
-                else: print('первое число не распознано, не удалось посчитать')
-            except:
-                print("Не могу посчитать")
-    if cmd == 'vikl':
-        os.system("taskkill /F /T /IM cmd.exe")
-    if cmd == 'comp':
+        break
+    elif text.count("искать в интернете") or text.count("ищи в интернете") or text.count("осуществи поиск в интернете") or text.count("осуществить поиск в интернете") or text.count("найти в интернете") or text.count("найди в интернете") or text.count("выполни поиск в интернете") or text.count("выполнить поиск в интернете") > 0:
+        s = text.split()
+        while s[0] != "интернете":
+            del s[0]
+        del s[0]
+        s = " ".join(s)
+        print("Ищу в интернете: " + s)
+        url = 'https://www.google.co.in/search?q='
+        search_url = url + s
+        webbrowser.open(search_url)
+        break
+    elif text.count("-") > 0:
+        s = text.split()
+        if s[0].isdigit() == True:
+            if s[2].isdigit() == True:
+                k = int(s[0]) - int(s[2])
+                print(str(s[0]) + " - " + str(s[2]) + " = " + str(k))
+            else:
+                print('Второе число не распознано, не удалось посчитать')
+        else:
+            print('Первое число не распознано, не удалось посчитать')
+        break
+    elif (text.count("выключи компьютер") > 0) or (text.count("выключить компьютер") > 0) or (text.count("выключай компьютер") > 0) or (text.count("заверши работу компьютера") > 0) or (text.count("завершить работу компьютера") > 0) or (text.count("завершай работу компьютера") > 0):
         os.system("shutdown -p")
-
-
-
-r = sr.Recognizer()
-m = sr.Microphone(device_index=1)
-
-print("Здравствуйте, повелитель")
-print("Практика слушает")
-
-stop_listening = r.listen_in_background(m, callback)
-while True: time.sleep(0.1)
+        break
+    elif (text.count("выключись") > 0) or (text.count("выключайся") > 0) or (text.count("выключить") > 0) or (text.count("выключить голосового ассистента") > 0) or (text.count("выключи голосового ассистента") > 0) or (text.count("выключай голосового ассистента") > 0) or (text.count("завершай работу голосового ассистента") > 0) or (text.count("заверши работу голосового ассистента") > 0) or (text.count("завершить работу голосового ассистента") > 0):
+        os.system("taskkill /F /T /IM cmd.exe")
+        break
+    else:
+        print("Команда не распознана")
+        break
